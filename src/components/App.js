7c670bd1bd33cdd3c3629dd66909ac575d63d63a
Aleksander Fret <alemik@op.pl>
2018-12-27 20:23:37 +0100

    App component, firebase test, dummy data

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,104 @@
 import React, { Component } from 'react';
-import classes from './App.module.scss';
-import firebase from '../config/firebaseConfig';
+import firebase from 'firebase/app';
+import db from '../config/firebaseConfig';
+import Toolbar from './Toolbar/Toolbar';
+import ShoppingLists from './ShoppingLists/ShoppingLists';
 
 class ShoppingList extends Component {
+  state = {
+    shoppingLists: [
+      {
+        name: 'codzienne',
+        color: 'red',
+        bought: 5,
+        total: 13,
+        type: 'complex',
+        shared: false,
+      },
+      {
+        name: 'warzywa',
+        color: 'blue',
+        bought: 2,
+        total: 14,
+        type: 'simple',
+        shared: false,
+      },
+      {
+        name: 'chemia',
+        color: 'green',
+        bought: 7,
+        total: 8,
+        type: 'simple',
+        shared: false,
+      },
+      {
+        name: 'prezenty',
+        color: 'orange',
+        bought: 1,
+        total: 4,
+        type: 'simple',
+        shared: true,
+      },
+    ],
+  };
+
   componentDidMount() {
-    const listA = [];
-    firebase.collection('shoppingLists')
-      .where('userId', '==', '1111111111').get()
-      .then((snapshot) => {
-        const list = [];
-        snapshot.forEach((doc) => {
-          list.push({ ...doc.data(), listId: doc.id });
-        });
-        console.log(list);
-      });
-    firebase.collection('complexLists')
-      .where('userId', '==', '1111111111').get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          listA.push(doc.data());
-        });
-        // console.log(listA[0].lists[0].id);
-        // firebase.collection('complexLists').doc(listA[0].lists[0].id).get()
-        //   .then((snap) => {
-        //     console.log(snap);
-        //     const refDocs = [];
-        //     // snap.forEach((doc) => {
-        //     //   refDocs.push(doc.data());
-        //     // });
-        //     console.log(refDocs);
-        //   });
-      });
-    firebase.doc('shoppingLists/MEsEc2znaCZPBAYgAP4U').get()
-      .then((snapshot) => {
-        console.log(snapshot.data());
-      });
+    // const listA = [];
+    // db.collection('simpleLists')
+    //   .where('creator', '==', '1111111111').get()
+    //   .then((snapshot) => {
+    //     const list = [];
+    //     snapshot.forEach((doc) => {
+    //       list.push({
+    //         ...doc.data(),
+    //         listId: doc.id,
+    //       });
+    //     });
+    //     console.log(list);
+    //   });
+    // db.collection('complexLists')
+    //   .where('creator', '==', '1111111111').get()
+    //   .then((snapshot) => {
+    //     snapshot.forEach((doc) => {
+    //       listA.push(doc.data());
+    //     });
+    //     listA[0].lists[0].get()
+    //       .then((snaps) => {
+    //         console.log({
+    //           ...snaps.data(),
+    //           listId: snaps.id,
+    //         });
+    //       });
+    //   });
+    // const userRef = db
+    //   .collection('simpleLists')
+    //   .doc('FTGskMPOxNREtEsjYSEu');
+    // console.log('dddddddddddddddddddddd', userRef);
+
+    // db.doc('simpleLists/MEsEc2znaCZPBAYgAP4U').get()
+    //   .then((snapshot) => {
+    //     console.log({
+    //       ...snapshot.data(),
+    //       listId: snapshot.id,
+    //     });
+    //   });
+
+    // const refff = db
+    //   .collection('complexLists')
+    //   .doc('C49zx8Kgvobx8OHLwSMr');
+    // console.log(refff);
+    // console.log(db);
+    // refff.update({ lists: firebase.firestore.FieldValue.arrayUnion(userRef) });
   }
 
   render() {
     return (
-      <div className={classes.ShoppingList}>Hello World!</div>
+      <React.Fragment>
+        <Toolbar />
+        <ShoppingLists
+          shoppingLists={this.state.shoppingLists}
+        />
+      </React.Fragment>
     );
   }
 }
