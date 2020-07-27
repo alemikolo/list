05c558780e010d7b81a3d449fe2dcc49ef994fa6
Aleksander Fret <olekfret80@gmail.com>
2020-07-27 19:38:51 +0200

    [L-2] Update package config to improve module resolving

--- packages/browser/src/app/components/Layout/Layout.tsx
+++ packages/browser/src/app/components/Layout/Layout.tsx
@@ -1,11 +1,22 @@
+/* eslint-disable no-console */
 import React, { FC } from 'react';
 
 import './Layout.scss';
+
+const getEvents = (): void => {
+  fetch('/api/lists/get', { method: 'GET' })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
 
 const Layout: FC = () => (
   <div className="layout">
     <h1>List App</h1>
     <p>Create your own list ęóąśłżźćń</p>
+    <button type="button" onClick={getEvents}>
+      server test
+    </button>
   </div>
 );
 
