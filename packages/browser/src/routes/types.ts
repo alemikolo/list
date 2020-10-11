509de9e323d04f16dd60903df2efb307d8ed1aee
Aleksander Fret <olekfret80@gmail.com>
2020-10-11 14:17:44 +0200

    [HIT-11] Install react-router-dom and create routes config

---
+++ packages/browser/src/routes/types.ts
@@ -0,0 +1,54 @@
+import Path from './enums';
+
+export type NoParams = undefined | void;
+export type AccountRoute = [Path.Account, NoParams];
+export type DashboardRoute = [Path.Dashboard, NoParams];
+export type HomeRoute = [Path.Home, NoParams];
+export type OrganizationMemberRoute = [
+  Path.OrganizationMember,
+  { memberId: string; organizationId: string }
+];
+export type OrganizationMembersRoute = [
+  Path.OrganizationMembers,
+  { organizationId: string }
+];
+export type OrganizationProjectRoute = [
+  Path.OrganizationProject,
+  { projectId: string; organizationId: string }
+];
+export type OrganizationProjectsRoute = [
+  Path.OrganizationProjects,
+  { organizationId: string }
+];
+export type OrganizationProjectTaskRoute = [
+  Path.OrganizationProjectTask,
+  { organizationId: string; projectId: string; taskId: string }
+];
+export type OrganizationRoute = [Path.Organization, { organizationId: string }];
+export type OrganizationsRoute = [Path.Organizations, NoParams];
+export type ProjectRoute = [Path.Project, { projectId: string }];
+export type ProjectsRoute = [Path.Projects, NoParams];
+export type SettingsRoute = [Path.Settings, NoParams];
+export type SignInRoute = [Path.SignIn, NoParams];
+export type SignUpRoute = [Path.SignUp, NoParams];
+export type TaskRoute = [Path.Task, { projectId: string; taskId: string }];
+
+type Route =
+  | AccountRoute
+  | DashboardRoute
+  | HomeRoute
+  | OrganizationMemberRoute
+  | OrganizationMembersRoute
+  | OrganizationProjectRoute
+  | OrganizationProjectsRoute
+  | OrganizationProjectTaskRoute
+  | OrganizationRoute
+  | OrganizationsRoute
+  | ProjectRoute
+  | ProjectsRoute
+  | SettingsRoute
+  | SignInRoute
+  | SignUpRoute
+  | TaskRoute;
+
+export default Route;
