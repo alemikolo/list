6dcf22c6a76e7b4a59d1fffc4d4601cdfb32b82b
Aleksander Fret <olekfret80@gmail.com>
2020-11-05 19:23:43 +0100

    [HIT-19] Refactor app's routes

--- packages/browser/src/app/App.tsx
+++ packages/browser/src/app/App.tsx
@@ -1,23 +1,22 @@
 import React, { FC } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 import Dashboard from 'modules/dashboard';
 import SignIn from 'modules/auth/SignIn';
 import SignUp from 'modules/auth/SignUp';
 import Bye from 'modules/user/Bye';
+import Home from 'modules/home';
 import { setAccessToken } from 'modules/auth/token';
 import useFetch from 'shared/hooks/useFetch';
 import Path from './routes/enums';
 import Layout from './components/Layout';
-import PrivateRoute from './routes/PrivateRoute';
+import { PrivateRoute, PublicRoute } from './routes';
 
 interface AT {
   accessToken: string;
 }
 const App: FC = () => {
-  const { data, pending, error } = useFetch<AT>(
-    'http://localhost:5000/api/auth/refresh-token'
-  );
+  const { data, pending, error } = useFetch<AT>('/api/auth/refresh-token');
 
   if (data) {
     const { accessToken } = data;
@@ -36,19 +35,11 @@ const App: FC = () => {
     <BrowserRouter>
       <Layout>
         <Switch>
-          <PrivateRoute
-            component={SignIn}
-            exact
-            isAuthenticated
-            path={Path.SignIn}
-          />
-          <Route component={SignUp} exact path={Path.SignUp} />
-          <Route component={Bye} exact path={Path.Organizations} />
-          <Route
-            component={Dashboard}
-            exact
-            path={[Path.Dashboard, Path.Home]}
-          />
+          <PublicRoute component={SignIn} exact path={Path.SignIn} />
+          <PublicRoute component={SignUp} exact path={Path.SignUp} />
+          <PublicRoute component={Home} exact path={Path.Home} />
+          <PrivateRoute component={Bye} exact path={Path.Organizations} />
+          <PrivateRoute component={Dashboard} exact path={Path.Dashboard} />
         </Switch>
       </Layout>
     </BrowserRouter>
---
+++ packages/browser/src/app/routes/Route.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from 'react';
+import {
+  Route,
+  Redirect,
+  RouteProps as SourceRouteProps,
+  RouteComponentProps
+} from 'react-router-dom';
+
+import { isAuthenticated } from 'modules/auth/utils';
+import Path from './enums';
+
+interface RouteProps extends SourceRouteProps {
+  noFallback?: boolean;
+  path?: Path;
+}
+
+interface BaseRouteProps extends RouteProps {
+  permitted: boolean;
+  redirectPath: Path;
+}
+
+const BaseRoute: FC<BaseRouteProps> = ({
+  component: Component,
+  permitted,
+  redirectPath,
+  render,
+  noFallback,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props: RouteComponentProps) => {
+      if (permitted) {
+        if (Component) {
+          return <Component {...props} />;
+        }
+
+        if (render) {
+          return render(props);
+        }
+      }
+
+      return noFallback ? null : (
+        <Redirect
+          to={{ pathname: redirectPath, state: { from: props.location } }}
+        />
+      );
+    }}
+  />
+);
+
+export const PrivateRoute: FC<RouteProps> = props => (
+  <BaseRoute
+    permitted={isAuthenticated()}
+    redirectPath={Path.SignIn}
+    {...props}
+  />
+);
+
+export const PublicRoute: FC<RouteProps> = props => (
+  <BaseRoute
+    permitted={!isAuthenticated()}
+    redirectPath={Path.Dashboard}
+    {...props}
+  />
+);
